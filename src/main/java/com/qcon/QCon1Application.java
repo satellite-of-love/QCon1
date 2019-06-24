afca20ad6d938cd2eb138268d57a4984153a924c
Jessica Kerr <jessitron@gmail.com>
2019-06-24 11:35:23 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/qcon/QCon1Application.java
@@ -0,0 +1,12 @@
+package com.qcon;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QCon1Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(QCon1Application.class, args);
+	}
+
+}
